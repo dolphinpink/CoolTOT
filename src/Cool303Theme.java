59be5e40950479e2cefb568d95bf4e4171121526
dolphin pink <dolphinpink@dolphinpink.dp>
2015-11-18 03:26:34 -0500

    actually resolved things

--- src/Cool303Theme.java
+++ src/Cool303Theme.java
@@ -1,14 +1,9 @@
 import java.awt.*;
 import javax.swing.*;
 
-<<<<<<< HEAD
-public abstract class Cool303Theme {
-	
-=======
 public interface Cool303Theme {
 	
 	public void shapeButton(Cool303Button button);
 	public void shapeWindow(Cool303Container window);
 	public void shapeWindow(Cool303Container window, String s);
->>>>>>> edaa1171fd1d3800f3a8641026466794314a1de6
 }
