feffd424345528a02ac0c1f570b1da70efd02923
dolphin pink <dolphinpink@dolphinpink.dp>
2015-11-18 01:11:11 -0500

    some minor changes

--- src/Cool303Container.java
+++ src/Cool303Container.java
@@ -3,4 +3,12 @@ import javax.swing.*;
 
 public class Cool303Container extends Container{
 
+	public Cool303Container(Cool303Theme theme, int size, String str) {
+		
+	}
+	
+	private void calculateMinSize() {
+		
+	}
+	
 }
--- src/Cool303Theme.java
+++ src/Cool303Theme.java
@@ -1,4 +1,4 @@
 
 public abstract class Cool303Theme {
-
+	
 }
